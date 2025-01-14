59f5cfff1523b8d7b867a5422ff3b293a9d22df3
vervebot apps <vervebotapps@192.168.1.18>
2025-01-14 15:36:30 +0530

    new changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const path = require(`path`)
 // const chunk = require(`lodash/chunk`)
 // const { Console } = require("console")
-// const { post } = require("jquery")
 /**
  * exports.createPages is a built-in Gatsby Node API.
  * It's purpose is to allow you to create pages for your site! 💡
