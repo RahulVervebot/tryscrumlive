1cfbe278629fde88c06381284d309707dcfeeab8
RahulVervebot <87413071+RahulVervebot@users.noreply.github.com>
2025-04-04 14:47:58 +0530

    Update StageofAgileTransformation.js

--- src/components/StageofAgileTransformation.js
+++ src/components/StageofAgileTransformation.js
@@ -61,7 +61,7 @@ const StageofAgileTransformation = () => {
      //  debugger
     //   setloader("loading")
       const url =
-        "https://tryscrumlive.vervebot.io//wp-json/contact-form-7/v1/contact-forms/8252/feedback"
+        "https://tryscrumlive.vervebot.io//wp-json/contact-form-7/v1/contact-forms/8252/feedback?_wpcf7_unit_tag=wpcf7-0c043f6"
       const formData = new FormData()
       formData.append("your-name", state.name)
       formData.append("your-email", state.email)
