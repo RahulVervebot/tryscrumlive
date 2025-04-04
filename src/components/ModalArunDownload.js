2aa38375c79b92ebcaca27125b5c5b2da564b903
RahulVervebot <87413071+RahulVervebot@users.noreply.github.com>
2025-04-04 14:56:07 +0530

    Update ModalArunDownload.js

--- src/components/ModalArunDownload.js
+++ src/components/ModalArunDownload.js
@@ -77,7 +77,7 @@ const ModalArunDownload = () => {
       debugger;
       setloader('loading');
       const url =
-        "https://tryscrumlive.vervebot.io//wp-json/contact-form-7/v1/contact-forms/7949/feedback";
+        "https://tryscrumlive.vervebot.io//wp-json/contact-form-7/v1/contact-forms/7949/feedback?_wpcf7_unit_tag=wpcf7-b792eb0";
       const formData = new FormData();
       formData.append("your-name", state.name);
       formData.append("your-email", state.email);
